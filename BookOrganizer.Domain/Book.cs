8b1c3d41ce1368f0f38aaa74ad79800aae1fb403
Toni Jaakkola <toni.jaakkola@gmail.com>
2018-11-13 19:53:48 +0200

    Book Title and two tests added. - Fluent assertions added. Tests refactored to use that.

--- BookOrganizer.Domain/Book.cs
+++ BookOrganizer.Domain/Book.cs
@@ -8,8 +8,25 @@ namespace BookOrganizer.Domain
 {
     public class Book
     {
+        private string _title;
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Books title should be at minimum 1 character long.")]
+        [MaxLength(256, ErrorMessage = "Books title should be maximum of 256 characters long.")]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value.Length < 1 || value.Length > 256)
+                    throw new ArgumentOutOfRangeException(nameof(Title), "Books title should be 1-256 characters long.");
+
+                _title = value;
+            }
+        }
 
     }
 }
--- BookOrganizer.DomainTests/BookTests.cs
+++ BookOrganizer.DomainTests/BookTests.cs
@@ -1,18 +1,40 @@
 using BookOrganizer.Domain;
 using System;
 using Xunit;
+using FluentAssertions;
 
 namespace BookOrganizer.DomainTests
 {
     public class BookTests
     {
         [Fact]
-        public void BookShouldBeIdentifiedByGuid()
+        public void BookShouldBeIdentifiedByValidGuid()
         {
             var book = new Book();
             book.Id = Guid.NewGuid();
+            book.Id.Should().NotBe(Guid.Empty);
+        }
 
-            Assert.NotEqual<Guid>(Guid.Empty, book.Id);
+        [Fact]
+        public void BookShouldAcceptTitlesLessThan256Characters()
+        {
+            var book = new Book();
+            book.Title = "My Book";
+
+            book.Title.Should().Equals("My Book");
+        }
+
+        [Fact]
+        public void BookTitleShouldNotBeLongerThan256Characters()
+        {
+            var book = new Book();
+
+            Action action = ()
+                => book.Title = "Spicy jalapeno bacon ipsum dolor amet prosciutto swine andouille hamburger tri-tip ground round pork " +
+                "belly. Capicola chuck andouille, short ribs turducken salami short loin filet mignon biltong pork belly fatback. " +
+                "Drumstick jowl pork chop, short ribs prosciutto picanha pork landjaeger pork loin.";
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
 }
