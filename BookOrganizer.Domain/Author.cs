b56c1791eac53e2d6d9be1dfa14f64619d44ff1e
Toni Jaakkola <toni.jaakkola@gmail.com>
2018-11-15 19:25:13 +0200

    Author and AuthorTests classes and Id property added

---
+++ BookOrganizer.Domain/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookOrganizer.Domain
+{
+    public class Author
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+    }
+}
---
+++ BookOrganizer.DomainTests/AuthorTests.cs
@@ -0,0 +1,21 @@
+﻿using BookOrganizer.Domain;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BookOrganizer.DomainTests
+{
+    public class AuthorTests
+    {
+        [Fact]
+        public void AuthorShouldBeIdentifiedByValidGuid()
+        {
+            var author = new Author();
+            author.Id = Guid.NewGuid();
+
+            author.Id.Should().NotBe(Guid.Empty);
+        }
+    }
+}
