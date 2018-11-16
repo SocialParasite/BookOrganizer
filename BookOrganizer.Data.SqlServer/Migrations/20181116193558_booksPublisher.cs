fdbb164c369afc59a9542aceff94f2bc47897a67
Toni Jaakkola <toni.jaakkola@gmail.com>
2018-11-16 21:37:02 +0200

    New migration added for publisher foreign key in book table

---
+++ BookOrganizer.Data.SqlServer/Migrations/20181116193558_booksPublisher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookOrganizer.Data.SqlServer.Migrations
+{
+    public partial class booksPublisher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PublisherId",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books",
+                column: "PublisherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PublisherId",
+                table: "Books");
+        }
+    }
+}
