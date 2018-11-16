380623b5540e46792b8b0d89e077738f6db058bd
Toni Jaakkola <toni.jaakkola@gmail.com>
2018-11-16 21:57:04 +0200

    BooksRead migration added

---
+++ BookOrganizer.Data.SqlServer/Migrations/20181116195449_BooksReadDate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookOrganizer.Data.SqlServer.Migrations
+{
+    public partial class BooksReadDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BooksReadDate",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newsequentialid()"),
+                    ReadDate = table.Column<DateTime>(nullable: false),
+                    BookId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BooksReadDate", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BooksReadDate_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BooksReadDate_BookId",
+                table: "BooksReadDate",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BooksReadDate");
+        }
+    }
+}
