e4366c57ebc2ed8ed0250732b7016265b4fd742a
Toni Jaakkola <toni.jaakkola@gmail.com>
2018-11-17 13:35:03 +0200

    Format class added

---
+++ BookOrganizer.Domain/Format.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookOrganizer.Domain
+{
+    public class Format
+    {
+        private string _name;
+        private string _abbreveation;
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Format name should be at minimum 1 character long.")]
+        [MaxLength(32, ErrorMessage = "Format name should be maximum of 32 characters long.")]
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value is null || value == string.Empty || value.Length < 1 || value.Length > 32)
+                    throw new ArgumentOutOfRangeException(nameof(Name), "Format name should be 1-32 characters long.");
+
+                _name = value;
+            }
+        }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Format abbreveation should be at minimum 1 character long.")]
+        [MaxLength(16, ErrorMessage = "Format abbreveation should be maximum of 16 characters long.")]
+        public string Abbreveation
+        {
+            get => _abbreveation;
+            set
+            {
+                if (value is null || value == string.Empty || value.Length < 1 || value.Length > 16)
+                    throw new ArgumentOutOfRangeException(nameof(Abbreveation), "Format name should be 1-16 characters long.");
+
+                _abbreveation = value;
+            }
+        }
+    }
+}
---
+++ BookOrganizer.DomainTests/FormatTests.cs
@@ -0,0 +1,77 @@
+﻿using BookOrganizer.Domain;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BookOrganizer.DomainTests
+{
+    public class FormatTests
+    {
+        [Fact]
+        public void FormatShouldAcceptNamesLessThan32Characters()
+        {
+            var format = new Format();
+            format.Name = "Portable Document Format";
+
+            format.Name.Should().BeOfType(typeof(string)).And.Equals("Portable Document Format");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void WhenFormatNameIsSetNullOrEmptyThrowsArgumentException(string name)
+        {
+            var format = new Format();
+
+            Action action = () => format.Name = name;
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void FormatNameShouldNotBeLongerThan32Characters()
+        {
+            var format = new Format();
+
+            Action action = ()
+                => format.Name = "Spicy jalapeno bacon ipsum dolor amet prosciutto swine andouille hamburger tri-tip ground round pork";
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+
+        [Fact]
+        public void FormatAbbreveationShouldAcceptNamesLessThan16Characters()
+        {
+            var format = new Format();
+            format.Abbreveation = "pdf";
+
+            format.Abbreveation.Should().BeOfType(typeof(string)).And.Equals("pdf");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void WhenFormatAbbreveationIsSetNullOrEmptyThrowsArgumentException(string abbreveation)
+        {
+            var format = new Format();
+
+            Action action = () => format.Abbreveation = abbreveation;
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void FormatAbbreveationShouldNotBeLongerThan16Characters()
+        {
+            var format = new Format();
+
+            Action action = ()
+                => format.Abbreveation = "portableformatdoc";
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
