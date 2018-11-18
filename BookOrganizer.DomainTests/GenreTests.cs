580e46feb1aa78cc0b80eb782d5171029188fd84
Toni Jaakkola <toni.jaakkola@gmail.com>
2018-11-18 14:43:57 +0200

    GenreTests - added one test - refactored some of the old ones

--- BookOrganizer.DomainTests/GenreTests.cs
+++ BookOrganizer.DomainTests/GenreTests.cs
@@ -10,7 +10,16 @@ namespace BookOrganizer.DomainTests
     public class GenreTests
     {
         [Fact]
-        public void BookMayBelongToDifferentGenres()
+        public void GenreShouldBeIdentifiedByValidGuid()
+        {
+            var genre = new Genre();
+            genre.Id = Guid.NewGuid();
+
+            genre.Id.Should().NotBe(Guid.Empty);
+        }
+
+        [Fact]
+        public void BookNameShouldAcceptStringsAsInput()
         {
             var genre = new Genre();
 
@@ -31,7 +40,7 @@ namespace BookOrganizer.DomainTests
         [Theory]
         [InlineData("")]
         [InlineData(null)]
-        public void WhenGenreNameIsSetNullOrEmptyThrowsArgumentException(string name)
+        public void WhenGenreNameIsSetNullOrEmpty_ThrowsArgumentOutOfRangeException(string name)
         {
             var genre = new Genre();
 
@@ -41,7 +50,7 @@ namespace BookOrganizer.DomainTests
         }
 
         [Fact]
-        public void GenreNameShouldNotBeLongerThan32Characters()
+        public void TryingToSetGenreNameLongerThan32Characters_ThrowsArgumentOutOfRangeException()
         {
             var genre = new Genre();
 
@@ -49,6 +58,19 @@ namespace BookOrganizer.DomainTests
                 => genre.Name = "Spicy jalapeno bacon ipsum dolor amet prosciutto swine andouille hamburger tri-tip ground round pork";
 
             action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void GenreShouldHaveReferenceToBooksBelongingUnderIt()
+        {
+            var genre = new Genre();
+            genre.BookLink = new List<BookGenres>
+            {
+                new BookGenres { Genre = genre, Book = new Book { Title = "Slow Regards of Silent Things" } },
+                new BookGenres { Genre = genre, Book = new Book { Title = "Queen of Fire" } }
+            };
+
+            genre.BookLink.Should().HaveCount(2);
         }
     }
 }
