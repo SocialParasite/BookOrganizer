ab602b13d8a828323302a90fe7757e5c6a518bc9
Toni Jaakkola <toni.jaakkola@gmail.com>
2019-03-26 18:45:02 +0200

    Added published book list into PublisherDetailViewModel and individual book selection (opens selected books DetailViewModel).

--- BookOrganizer.UI.WPF/ViewModels/PublisherDetailViewModel.cs
+++ BookOrganizer.UI.WPF/ViewModels/PublisherDetailViewModel.cs
@@ -1,18 +1,22 @@
 ﻿using BookOrganizer.Domain;
+using BookOrganizer.UI.WPF.Events;
 using BookOrganizer.UI.WPF.Repositories;
 using BookOrganizer.UI.WPF.Services;
+using Prism.Commands;
 using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BookOrganizer.UI.WPF.ViewModels
 {
     public class PublisherDetailViewModel : BaseDetailViewModel<Publisher>, IPublisherDetailViewModel
     {
         private string name;
+        private Guid selectedBookId;
 
         public PublisherDetailViewModel(IEventAggregator eventAggregator,
             IMetroDialogService metroDialogService,
@@ -21,14 +25,38 @@ namespace BookOrganizer.UI.WPF.ViewModels
         {
             Repository = publisherRepo ?? throw new ArgumentNullException(nameof(publisherRepo));
 
+            ShowSelectedBookCommand = new DelegateCommand<Guid?>(OnShowSelectedBookExecute, OnShowSelectedBookCanExecute);
             SelectedItem = new Publisher();
 
         }
+
+        private bool OnShowSelectedBookCanExecute(Guid? id)
+            => (id is null || id == Guid.Empty) ? false : true;
+
+        private void OnShowSelectedBookExecute(Guid? id)
+            => SelectedBookId = (Guid)id;
+
+        public ICommand ShowSelectedBookCommand { get; set; }
 
         public string Name
         {
             get { return name; }
             set { name = value; OnPropertyChanged(); TabTitle = value; SelectedItem.Name = value; }
+        }
+
+        public Guid SelectedBookId
+        {
+            get => selectedBookId;
+            set
+            {
+                selectedBookId = value;
+                OnPropertyChanged();
+                if (selectedBookId != Guid.Empty)
+                {
+                    eventAggregator.GetEvent<OpenItemMatchingSelectedBookIdEvent<Guid>>()
+                                   .Publish(selectedBookId);
+                }
+            }
         }
 
         public async override Task LoadAsync(Guid id)
