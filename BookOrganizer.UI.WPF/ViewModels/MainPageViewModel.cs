f4e130a1f6fbd023b55d922bb8d30a8fbed319bb
Toni Jaakkola <toni.jaakkola@gmail.com>
2019-05-23 17:17:19 +0300

    Add button functionality added to MainPageViewModel

--- BookOrganizer.UI.WPF/ViewModels/MainPageViewModel.cs
+++ BookOrganizer.UI.WPF/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BookOrganizer.UI.WPF.Events;
+using Prism.Commands;
+using Prism.Events;
+using System;
+using System.Windows.Input;
 
 namespace BookOrganizer.UI.WPF.ViewModels
 {
     public class MainPageViewModel : IMainPageViewModel
     {
+        private readonly IEventAggregator eventAggregator;
+
+        public MainPageViewModel(IEventAggregator eventAggregator)
+        {
+            this.eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
+
+            AddNewItemCommand = new DelegateCommand<Type>(OnAddNewItemExecute);
+            EditLanguagesCommand = new DelegateCommand(OnEditLanguagesExecute);
+        }
+
+
+        // Taken from BaseViewModel. Repeat or create a new base for just this?
+        public ICommand AddNewItemCommand { get; }
+        public ICommand EditLanguagesCommand { get; }
+
+        private void OnAddNewItemExecute(Type itemType)
+        {
+            eventAggregator.GetEvent<OpenDetailViewEvent>()
+                       .Publish(new OpenDetailViewEventArgs
+                       {
+                           Id = new Guid(),
+                           ViewModelName = Type.GetType(itemType.FullName).Name
+                       });
+        }
+
+        private void OnEditLanguagesExecute()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
