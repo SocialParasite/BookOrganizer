0c58efe30c419da3808d9e3fb58457f0deae5674
Toni Jaakkola <toni.jaakkola@gmail.com>
2019-11-10 11:09:56 +0200

    prop rename
    biography => _biography in Author

--- BookOrganizer.UI.WPF/ViewModels/BaseDetailViewModel.cs
+++ BookOrganizer.UI.WPF/ViewModels/BaseDetailViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using BookOrganizer.Domain;
 using BookOrganizer.UI.WPF.Enums;
 using BookOrganizer.UI.WPF.Events;
-using BookOrganizer.DA;
 using BookOrganizer.UI.WPF.Services;
 using MahApps.Metro.Controls.Dialogs;
 using Prism.Commands;
