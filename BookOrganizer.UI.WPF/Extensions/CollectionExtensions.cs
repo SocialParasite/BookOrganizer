38da67baa172c2ac19cf039fe9619101a7f0c06b
Toni Jaakkola <toni.jaakkola@gmail.com>
2019-11-30 15:15:00 +0200

    Search added to publishers view
    Search box added to publishers view, ListToList extension method added

---
+++ BookOrganizer.UI.WPF/Extensions/CollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOrganizer.UI.WPF.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static List<T> FromListToList<T>(this IEnumerable<T> enumerableList)
+        {
+            return enumerableList != null ? new List<T>(enumerableList) : null;
+        }
+    }
+
+}
