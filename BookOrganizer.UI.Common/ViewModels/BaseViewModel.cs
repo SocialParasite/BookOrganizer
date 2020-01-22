b340cf4ad1dadc50c133d274af76c4b9530ea0f8
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-01-22 12:10:19 +0200

    Ui.Common
    BaseViewModel copied from UI.WPF, will require changes.
    
    CollectionExtension copied to UI.Common
    
    LangVersion changed to 8.0 for Web.UI

---
+++ BookOrganizer.UI.Common/ViewModels/BaseViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookOrganizer.DA;
+using BookOrganizer.Domain;
+using BookOrganizer.UI.Common.Extensions;
+
+namespace BookOrganizer.UI.Common.ViewModels
+{
+    public abstract class BaseViewModel<T> where T : class, IIdentifiable
+    {
+        private List<LookupItem> entityCollection;
+        private string searchString;
+
+        public IEnumerable<LookupItem> Items;
+        //public IRepository<T> Repository;
+
+        public List<LookupItem> EntityCollection
+        {
+            get => entityCollection;
+            set
+            {
+                entityCollection = value;
+                FilteredEntityCollection = entityCollection.FromListToList();
+                //OnPropertyChanged();
+            }
+        }
+
+        private List<LookupItem> filteredEntityCollection;
+
+        public List<LookupItem> FilteredEntityCollection
+        {
+            get => filteredEntityCollection;
+            set
+            {
+                filteredEntityCollection = value;
+                //OnPropertyChanged();
+            }
+        }
+
+        public string SearchString
+        {
+            get { return searchString; }
+            set
+            {
+                searchString = value;
+                //OnPropertyChanged();
+                UpdateFilteredEntityCollection();
+            }
+        }
+
+        private void UpdateFilteredEntityCollection()
+        {
+            FilteredEntityCollection.Clear();
+            FilteredEntityCollection = EntityCollection.Where(w => w.DisplayMember
+                                                       .IndexOf(SearchString, StringComparison.OrdinalIgnoreCase) != -1)
+                                                       .ToList();
+        }
+
+        public abstract Task InitializeRepositoryAsync();
+    }
+}
