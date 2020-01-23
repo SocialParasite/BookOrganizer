09f30c242d3668199b150ed0b43138e5a7df7499
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-01-23 18:50:09 +0200

    SQL scripts for misc statistics
    Added first script as a test.

---
+++ Book_Statistics.sql
@@ -0,0 +1,12 @@
+﻿SELECT		YEAR(br.ReadDate)									AS 'Year',
+			COUNT(*)											AS 'Books read',
+			MIN(b.PageCount)									AS 'Shortest book',
+			MAX(b.PageCount)									AS 'Longest book',
+			SUM(b.PageCount)									AS 'Total pages', 
+			IIF(YEAR(br.ReadDate) = YEAR(GETDATE()), 
+				SUM(b.PageCount)/(SELECT MONTH(GETDATE())), 
+				SUM(b.PageCount)/12)							AS 'Monthly average',
+			AVG(b.PageCount)									AS 'Average book length'
+FROM		Books b
+JOIN		BooksReadDate br ON BookId = b.Id
+GROUP BY	YEAR(br.ReadDate)
