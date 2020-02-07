3e1ea8892d2c5d164a464c871913b00c2c0c1cbf
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-02-07 10:21:45 +0200

    Book statistics sql script modified
    pages/month now takes into consideration of the current date, so if month has just started it doesn't count it as a whole month and thus incorrectly calculating the average.

--- Book_Statistics.sql
+++ Book_Statistics.sql
@@ -4,7 +4,14 @@
 			MAX(b.PageCount)									AS 'Longest book',
 			SUM(b.PageCount)									AS 'Total pages', 
 			IIF(YEAR(br.ReadDate) = YEAR(GETDATE()), 
-				SUM(b.PageCount)/(SELECT MONTH(GETDATE())), 
+				CAST
+				(
+					SUM(b.PageCount)/
+						(SELECT CAST(DAY(GETDATE()) AS DECIMAL)/
+								CAST(DAY(EOMONTH(GETDATE())) AS DECIMAL)
+								+ MONTH(GETDATE()) - 1) 
+					AS INT
+				), 
 				SUM(b.PageCount)/12)							AS 'Monthly average',
 			AVG(b.PageCount)									AS 'Average book length'
 FROM		Books b
