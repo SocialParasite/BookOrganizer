3403749fefe8e03439197a7e21e8b4f92d2b397b
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-03-19 14:33:45 +0200

    Range report
    TotalRowIdentifier converter added so that summary row now shows  word "Total" in first column instead of 0.

---
+++ BookOrganizer.UI.WPFCore/Converters/TotalRowIdentifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BookOrganizer.UI.WPFCore.Converters
+{
+    public class TotalRowIdentifier : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 0)
+            {
+                return "Total";
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
